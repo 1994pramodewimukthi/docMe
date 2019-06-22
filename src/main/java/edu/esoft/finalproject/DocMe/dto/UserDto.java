11b9f4b06bd30814b26fca88c0d03b3682a4ff77
Anuruddha <anuruddha@mi-synergy.com>
2019-06-22 10:57:23 +0530

    document upload incomplete

--- src/main/java/edu/esoft/finalproject/DocMe/dto/UserDto.java
+++ src/main/java/edu/esoft/finalproject/DocMe/dto/UserDto.java
@@ -11,6 +11,8 @@ public class UserDto {
     private String mobile;
     private String nic;
     private String email;
+    private String userName;
+    private String password;
 
     public String getId() {
         return id;
@@ -92,5 +94,21 @@ public class UserDto {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
