cb149f088877efa86dca8f56966440510b60238f
sandun <sandun@mi-synergy.com>
2019-06-22 18:01:44 +0530

    doc upload changes

--- src/main/java/edu/esoft/finalproject/DocMe/config/CommonFunction.java
+++ src/main/java/edu/esoft/finalproject/DocMe/config/CommonFunction.java
@@ -6,12 +6,9 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+
 
 @Component
 public class CommonFunction {
@@ -24,8 +21,8 @@ public class CommonFunction {
             File convFile = new File(multipartFile.getOriginalFilename());
             convFile.createNewFile();
             FileOutputStream fos = new FileOutputStream(convFile);
-            fos.write(multipartFile.getBytes());
-            fos.close();
+                fos.write(multipartFile.getBytes());
+                fos.close();
             convFile = convFile;
             MagicMatch match = Magic.getMagicMatch(convFile, true, true);
             String magicType = new String(match.getTest().array());
@@ -44,7 +41,6 @@ public class CommonFunction {
 
     public boolean isFileSizeSufficient(MultipartFile multipartFile, Long maxSize) { //size should be MB
         try {
-//            Long fileSize = multipartFile.getSize() / (1024 * 1024);
             Long fileSize = multipartFile.getSize();
             return fileSize > maxSize*(1024 * 1024) ? false : true;
         }catch (Exception e){
