ccefaa038e1ce61dc9f8300947764b6824741969
Anuruddha <anuruddha@mi-synergy.com>
2019-06-26 11:31:39 +0530

    redelect root location

---
+++ src/main/java/edu/esoft/finalproject/DocMe/contoller/MainController.java
@@ -0,0 +1,13 @@
+package edu.esoft.finalproject.DocMe.contoller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class MainController {
+    @RequestMapping(value = "/")
+    public ModelAndView rootLocation() {
+        return new ModelAndView("redirect:/ui/login");
+    }
+}
